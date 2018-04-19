3c59118fade4f1ea26063662bb6e69936c329a3f
Rodrigo <rodizipa2007@gmail.com>
2018-04-19 12:28:57 -0300

    fixing the 3 arguments case

--- Commands/cmd_assign_role.py
+++ Commands/cmd_assign_role.py
@@ -4,13 +4,18 @@ import discord
 
 async def ex(message, client):
     parsed_message = message.content.replace(CONFIG.PREFIX + "arole", "")[1:]
-
     if message.author.id == 114010253938524167:
         list = parsed_message.split(",")
-        user_roles = message.mentions[0].roles[:]
-        new_role = discord.utils.get(message.guild.roles, name=list[1])
-        await message.mentions[0].add_roles(new_role)
-        await message.delete()
+        if len(list) >2:
+            if list[2]=="remove":
+                user_roles = message.mentions[0].roles[:]
+                new_role = discord.utils.get(message.guild.roles, name=list[1])
+                await message.mentions[0].remove_roles(new_role)
+        else:
+            user_roles = message.mentions[0].roles[:]
+            new_role = discord.utils.get(message.guild.roles, name=list[1])
+            await message.mentions[0].add_roles(new_role)
+            await message.delete()
     else:
         await message.author.send("You don't have permissions to do that.")
         await message.delete()

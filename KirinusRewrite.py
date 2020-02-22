3c8dcb328e8ec1b612af8a76f90d125a2f2ee52b
Rodrigo <rodizipa2007@gmail.com>
2020-02-22 13:19:18 -0300

    Added error logs, removing unused stuff

--- KirinusRewrite.py
+++ KirinusRewrite.py
@@ -1,75 +1,28 @@
-import discord
-from discord.ext import commands
 import asyncio
-import asyncpg
+import logging
 from time import gmtime, strftime
-from utils import formatter
-import CONFIG
-import pendulum
 
+import asyncpg
+import discord
+import pendulum
+from discord.ext import commands
+
+import CONFIG
+from utils import formatter
+
+logging.basicConfig(level=logging.WARN, filename='error.log')
 
 class Bot(commands.Bot):
     def __init__(self, **kwargs):
         super().__init__(command_prefix=CONFIG.PREFIX, description="Destiny Child KR bot.")
         self.db = kwargs.pop("db")
         self.rt = self.loop.create_task(self.reset_task())
-        self.nick_pool = []  # For april's fools event
 
     async def reset_task(self):
         await self.wait_until_ready()
-        claim_reset = None
-        rolls_reset = None
-        decay_affinity = None
 
         while not self.is_closed():
-            query = "select * from alarms"
-            rows = await self.db.fetch(query)
-
-            for item in rows:
-                if item['alarm_name'] == 'claim_reset':
-                    claim_reset = pendulum.instance(item['alarm_time'])
-                elif item['alarm_name'] == 'rolls_reset':
-                    rolls_reset = pendulum.instance(item['alarm_time'])
-                elif item['alarm_name'] == 'decay_affinity':
-                    decay_affinity = pendulum.instance(item['alarm_time'])
-
             now = pendulum.now()
-
-            if now > claim_reset:
-                claim_reset = claim_reset.add(hours=6)
-                claim_reset = formatter.pendulum_to_datetime(claim_reset)
-
-                connection = await self.db.acquire()
-                async with connection.transaction():
-                    update = "UPDATE alarms SET alarm_time=$1  WHERE alarm_name=$2;"
-                    await self.db.execute(update, claim_reset, 'claim_reset')
-                    update = "UPDATE w_players SET claim = 1 WHERE claim < 1"
-                    await self.db.execute(update)
-                await self.db.release(connection)
-
-            if now > rolls_reset:
-                rolls_reset = rolls_reset.add(hours=1)
-                rolls_reset = formatter.pendulum_to_datetime(rolls_reset)
-
-                connection = await self.db.acquire()
-                async with connection.transaction():
-                    update = "UPDATE alarms SET alarm_time = $1  WHERE alarm_name = $2"
-                    await self.db.execute(update, rolls_reset, 'rolls_reset')
-                    update = "UPDATE w_players SET rolls = 5 WHERE rolls != 5"
-                    await self.db.execute(update)
-                await self.db.release(connection)
-
-            if now > decay_affinity:
-                decay_affinity = decay_affinity.add(hours=4, minutes=48)
-                decay_affinity = formatter.pendulum_to_datetime(decay_affinity)
-                connection = await self.db.acquire()
-                async with connection.transaction():
-                    update = "UPDATE alarms SET alarm_time = $1  WHERE alarm_name = $2"
-                    await self.db.execute(update, decay_affinity, 'decay_affinity')
-                    update = "UPDATE w_card SET affinity = affinity-20 where owner is not NULL"
-                    await self.db.execute(update)
-                    await self.db.execute("UPDATE w_card SET owner = NULL, affinity = 100 WHERE affinity < 1")
-                await self.db.release(connection)
 
             # Process role time assignment
             role_time = await self.db.fetch("select * from assign_roles")
@@ -85,7 +38,7 @@ class Bot(commands.Bot):
 
                     check_time = pendulum.instance(item['time'])
 
-                    if (now > check_time):
+                    if now > check_time:
 
                         # remove role (assign plankton if dunce)
                         if member:
@@ -125,7 +78,7 @@ class Bot(commands.Bot):
         await self.change_presence(activity=discord.Game('?help or Die!'))
 
     async def load_modules(self):
-        modules = ['cogs.dbstuff', 'cogs.admin', 'cogs.fun', 'cogs.dc', 'cogs.waifugame']
+        modules = ['cogs.dbstuff', 'cogs.admin', 'cogs.fun', 'cogs.dc', 'cogs.karma']
 
         for extension in modules:
             self.load_extension(extension)
@@ -142,13 +95,15 @@ class Bot(commands.Bot):
                 if role:
                     await formatter.kirinus_gacha(message)
 
-            except Exception:
-                pass
+            except discord.DiscordException as ex:
+                print("An discord error occurred. Logging.")
+                logging.error(ex.__name__, ex)
 
 
 async def run():
+
     pg_credentials = {"user": CONFIG.USERNAME, "password": CONFIG.PASSWORD, "database": CONFIG.DATABASE,
-                      "host": "127.0.0.1"}
+                      "host": CONFIG.HOST}
     async with asyncpg.create_pool(**pg_credentials) as db:
         bot = Bot(db=db)
         try:
@@ -163,5 +118,5 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(run())
-    except Exception as e:
-        print(e.__name__)
+    except discord.DiscordException as de:
+        logging.error(de.__name__)
